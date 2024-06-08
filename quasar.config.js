b93c90c8ef11e5176234a86cf24da849ad1eb5a4
Иван Илюшин <iilyushin@samokat.ru>
2024-06-08 22:08:32 +0300

    VITE_APP_PATH

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function(/* ctx */) {
 
 			// rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-			// publicPath: '/',
+			publicPath: '/freeze-light/',
 			// analyze: true,
 			// env: {},
 			// rawDefine: {}
