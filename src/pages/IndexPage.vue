0aa203039f937ff9708042d3217be63a34a24b14
Иван Илюшин <iilyushin@samokat.ru>
2024-06-08 22:12:06 +0300

    VITE_APP_PATH

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <p>Описание</p>
+    <router-link to="main">START</router-link>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
